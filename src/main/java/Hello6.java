9a9cdfa125f44023fd60d8e979933865dbfdd3dd
dran0id <xixjsfsj@gmail.com>
2026-03-13 20:12:34 +0900

    오늘 마지막인데 마지막이 아닌... 예습 조금

--- src/main/java/Hello6.java
+++ src/main/java/Hello6.java
@@ -1,7 +1,11 @@
 public class Hello6 {
+    public static void main(String [] arge){
+        int num1, num2, result;
+        num1 =30;
+        num2 =40;
 
-    public static void main(String[] args) {
-     System.out.println("아이야이야");
+        result = num1 - num2;
+        System.out.println(num1 + "-" + num2+ "=" + result);
 
     }
 }
