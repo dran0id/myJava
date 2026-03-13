0853a4f0de444ff448e327ca5c16549b56ed383f
unknown <xixjsfsj@gmail.com>
2026-03-13 15:04:50 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,21 @@
+public class Hello{
+
+
+    public static void main(String[] args){
+
+        System.out.println("123");
+
+        System.out.println("456");
+
+        System.out.println("789");
+
+        System.out.println("222");
+
+        System.out.println("333");
+
+        System.out.println("444");
+
+        System.out.println("555");
+
+    }
+}
