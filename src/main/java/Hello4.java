dbd6d96b227ea879e44c58833877d2b9acdca15b
dran0id <xixjsfsj@gmail.com>
2026-03-13 15:48:54 +0900

    외워야한다!!

---
+++ src/main/java/Hello4.java
@@ -0,0 +1,13 @@
+public class Hello4 {
+    public static void main(String[] args){
+
+        System.out.println("이걸로 네번째다 와아");
+
+        System.out.println("이걸로 네번째!");
+
+        System.out.println("사실 세번째");
+
+        System.out.println("진짜 네번째다");
+
+    }
+}
