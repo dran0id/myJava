226adc72ee729611c1d26644d24fe0684b26ff91
dran0id <xixjsfsj@gmail.com>
2026-03-27 15:28:55 +0900

    연습-2(수식) 03/27

---
+++ src/main/java/Code03_09.java
@@ -0,0 +1,12 @@
+public class Code03_09 {
+    public static void main(String [] args){
+        int b = 20;
+        b++ ; System.out.print(b+" ");
+        b-- ; System.out.print(b+" ");
+        b += 3 ; System.out.print(b+ " ");
+        b -= 3 ; System.out.print(b+ " ");
+        b *= 3 ; System.out.print(b+ " ");
+        b /= 3 ; System.out.print(b+ " ");
+        b %= 3 ; System.out.print(b+ " ");
+    }
+}
