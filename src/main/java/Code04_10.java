91fe1692de74cea416f0c62967884bc90c55f74c
dran0id <xixjsfsj@gmail.com>
2026-04-02 13:56:03 +0900

    연습-3 04/02

---
+++ src/main/java/Code04_10.java
@@ -0,0 +1,11 @@
+public class Code04_10 {
+    public static void main(String [] args){
+        String a="난생처음 자바";
+        int b;
+
+        b= a.length();
+
+        System.out.println("문자열 : " + a);
+        System.out.println("문자열 길이 :" + b);
+    }
+}
