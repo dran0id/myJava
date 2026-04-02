460800ca48e4dd3e743cf472c9fabe32f7e94126
dran0id <xixjsfsj@gmail.com>
2026-04-02 13:55:33 +0900

    연습- 04/02

---
+++ src/main/java/Code04_07.java
@@ -0,0 +1,9 @@
+public class Code04_07 {
+    public static void main(String [] args){
+        String a="작은 따옴표는 \' 모양입니다.";
+        String b="큰 따옴표는 \" 모양입니다.";
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
