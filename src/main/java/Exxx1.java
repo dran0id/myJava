a709b51fb7318e0a18eec999e1d2969bf3214d36
dran0id <xixjsfsj@gmail.com>
2026-04-10 11:03:19 +0900

    for문 이해를 위한 한걸음 26/4/9

---
+++ src/main/java/Exxx1.java
@@ -0,0 +1,9 @@
+public class Exxx1 {
+    public static void main(String[] args){
+        int hap = 2;
+        for (int i=1; i<=10; i++){
+            hap= hap * i;
+        }
+        System.out.printf("어떻게 나오는지 실험 ㄱㄱ:"+hap);
+    }
+}
