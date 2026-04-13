799c138a5949b712eaa83d8a3d21585cc13196c2
dran0id <xixjsfsj@gmail.com>
2026-04-13 12:34:19 +0900

    저번에 배운거 올림 26/04/13

---
+++ src/main/java/Code06_04.java
@@ -0,0 +1,13 @@
+public class Code06_04 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 10; i++,i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int w = 2; w <= 10; w += 2) {
+            System.out.print(w + " ");
+        }
+    }
+}
+
